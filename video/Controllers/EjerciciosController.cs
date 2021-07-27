d6eb8eeee229184867a2f530478f84176725650b
mihaelcruz <mihael.cruz@gmail.com>
2021-07-26 20:31:35 -0400

    Agregar archivos de proyecto.

---
+++ video/Controllers/EjerciciosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace video.Controllers
+{
+    public class EjerciciosController : Controller
+    {
+        // GET: Ejercicios
+        public ActionResult Index()
+        {
+            int[] num = {5, 1, 7, 3,8,4,10,11,12,9};
+            Array.Sort(num);
+            ViewBag.num=num;
+            return View();
+        }
+    }
+}
---
+++ video/Controllers/PeliculasController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using video.Data;
+
+namespace video.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private videoEntities db = new videoEntities();
+
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View(db.Peliculas.ToList());
+            }
+            else {
+                return Redirect("/Home");
+            }
+            
+        }
+
+        // GET: Peliculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Peliculas peliculas = db.Peliculas.Find(id);
+            if (peliculas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peliculas);
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,Codigo,Descripcion")] Peliculas peliculas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peliculas.Add(peliculas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(peliculas);
+        }
+
+        // GET: Peliculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Peliculas peliculas = db.Peliculas.Find(id);
+            if (peliculas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peliculas);
+        }
+
+        // POST: Peliculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,Codigo,Descripcion")] Peliculas peliculas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(peliculas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(peliculas);
+        }
+
+        // GET: Peliculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Peliculas peliculas = db.Peliculas.Find(id);
+            if (peliculas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peliculas);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Peliculas peliculas = db.Peliculas.Find(id);
+            db.Peliculas.Remove(peliculas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
